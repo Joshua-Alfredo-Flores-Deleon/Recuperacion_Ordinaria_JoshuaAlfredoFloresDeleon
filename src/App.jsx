8c5451a48b9051df9194fb713dc73a103342e114
pepon <20210261@ricaldone.edu.sv>
2026-08-19 11:54:57 -0600

    Proyecto casi terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Inicio from "./pages/dashboard"
-import Administracion  from './pages/administracon'
+import Administracion  from './pages/administracion'
 import Navbar from './components/navbar'
 
 function App() {
---
+++ src/pages/administracion.jsx
@@ -0,0 +1,121 @@
+import "../administracion.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import formAdministracion from "../components/formAdministracion";
+import Navbar from "../Navbar.css"
+
+const API_URI = "https://retoolapi.dev/U5EBDb/Estudiantes";
+
+function administracion() {
+  const [estudiates, setEstudiantes] = useState();
+  const [filtered, setFiltered] = useState();
+  const [search, setSearch] = useState();
+  const [loading, setLoading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
+  const [editEstudiante, setEditEstudiante] = useState(null);
+
+  const fetchEstudiantes = async () => {
+    try {
+      const res = await axios.get(`${API_URI}/U5EBDb/Estudiantes`);
+      setEstudiantes(res.data);
+      setFiltered(res.data);
+    } catch (err) {
+      console.error("error fetching Estudiantes: ", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  (useEffect) => {
+    if (search.trim() === "") {
+      setFiltered(estudiates);
+    } else {
+      const term = search.toLoweCase();
+      setFiltered(
+        estudiates.filter(
+          (p) =>
+            p.carnet?.toLoweCase().includes(term) ||
+            p.nombre?.toLoweCase().includes(term) ||
+            p.apellido?.toLoweCase().includes(term) ||
+            p.grado?.toLoweCase().includes(term) ||
+            p.estado?.toLoweCase().includes(term),
+        ),
+      );
+    }
+  };
+  [search, estudiates];
+
+  const handleSave = async (data) => {
+    try {
+      if (editEstudiante) {
+        await axios.put(
+          `${API_URI}/U5EBDb/Estudiantes/${editEstudiante._id}, data`,
+        );
+      } else {
+        await axios.post(`${API_URI}/U5EBDb/Estudiantes`, data);
+        setShowForm(false);
+        setEditEstudiante(null);
+        fetchEstudiantes();
+      }
+    } catch (err) {
+      console.log("error saving Estudiante: ", err);
+      alert("error al guardar el estudiante");
+    }
+  };
+
+  const handleDelete = async (id) => {
+    if (!confirm("¿Estas seguro de eliminar este producto?"))
+
+    try {
+      await axios.delete(`${API_URI}/U5EBDb/Estudiantes`);
+        fetchEstudiantes
+    } catch {
+        console.error('Error deleting Estudiantre: ', err)
+        alert('Error al eliminar el producto')
+    }
+
+    const handleOpenForm = () => {
+      setEditEstudiante(null);
+      setShowForm(true);
+    };
+  };
+}
+
+if(loading){    
+    return <div>Cargando Productos...</div>
+}
+
+return (
+    <>
+    <Navbar/>
+        <div>
+            <Boton texto="Agregar" onClick={handleOpenForm}/>
+            <div>
+                <input 
+                    type="text" 
+                    placeholder="Buscar Usuario"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    id="search-Estudiantes"
+                />
+                <fiSearch/>
+            </div>
+        </div>
+
+        <formAdministracion
+            estudiates={filtered}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+        />
+        {showForm && (
+            <formAdministracion
+                estudiates = {editEstudiante}
+                onSave={handleSave}
+                onClose={() => setShowForm(false)}
+            />
+        )
+    }
+    </>
+)
+
+export default administracion;
